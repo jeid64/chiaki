35798fe1ecd17db9f4dd3bc5e09380020f317607
Ross Delinger <rossdylan@fastmail.com>
2022-03-17 15:14:37 -0700

    [build] Push ffmpeg forward to 5.0

--- scripts/build-ffmpeg.sh
+++ scripts/build-ffmpeg.sh
@@ -5,10 +5,10 @@ cd "./$1"
 shift
 ROOT="`pwd`"
 
-TAG=n4.3.1
+TAG=n5.0
 
 git clone https://git.ffmpeg.org/ffmpeg.git --depth 1 -b $TAG && cd ffmpeg || exit 1
 
-./configure --disable-all --enable-avcodec --enable-decoder=h264 --enable-decoder=hevc --enable-hwaccel=h264_d3d11va --enable-hwaccel=hevc_d3d11va --enable-dxva2 --enable-d3d11va --prefix="$ROOT/ffmpeg-prefix" "$@" || exit 1
+./configure --disable-all --enable-libx264 --enable-libx265 --enable-avcodec --enable-decoder=h264 --enable-decoder=hevc --enable-hwaccel=h264_d3d11va --enable-hwaccel=hevc_d3d11va --enable-dxva2 --enable-d3d11va --prefix="$ROOT/ffmpeg-prefix" "$@" || exit 1
 make -j4 || exit 1
 make install || exit 1
